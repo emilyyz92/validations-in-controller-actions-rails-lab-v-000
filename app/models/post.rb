6e63bdac918c25cda15bacb0c07391b9b4bcd638
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-27 20:28:10 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 1}
   validates :email, uniqueness: true
+  validates :category, inclusion: {in: %w(Fiction, Non-Fiction)}
 end
