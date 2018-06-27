375bf6e4956bbc7d12c10fc9b38116b701b45659
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-27 20:34:09 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true, message: "Username Required"
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, message: "This email is already registered"
 end
